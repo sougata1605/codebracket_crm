14c9a9dd110edcaabd7912da6ef3600e8535c74d
Sougata Chatterjee <chatterjee2014@gmail.com>
2025-12-30 18:59:19 +0530

    login and logout check

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,36 @@
       crossorigin="subhas shanta chatterjee"
       referrerpolicy="no-referrer" />
 
+<style>
+  #festivalGreeting {
+    font-size: 16px;
+    color: #ff9800;
+    font-weight: bold;
+    text-align: center;
+    margin: 20px;
+  }
+
+  .balloon {
+    width: 20px;
+    height: 30px;
+    background-color: red;
+    border-radius: 50% 50% 50% 50%;
+    position: absolute;
+    animation: float 5s infinite;
+  }
+
+  @keyframes float {
+    0% { transform: translateY(0) rotate(0deg); }
+    50% { transform: translateY(-300px) rotate(180deg); }
+    100% { transform: translateY(0) rotate(360deg); }
+  }
+</style>
+
+
+
+
+
+
 </head>
 <body>
 
