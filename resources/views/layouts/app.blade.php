44cce0e10023e0c3e2e600f1a6feed4dcaecf132
Sougata Chatterjee <chatterjee2014@gmail.com>
2025-12-31 18:16:39 +0530

    lead related issues resolved

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,10 +2,6 @@
 <html>
 
 <head>
-
-
-
-
   <title>
     @hasSection('title')
     @yield('title') | {{ config('app.name') }}
@@ -21,55 +17,7 @@
     crossorigin="subhas shanta chatterjee"
     referrerpolicy="no-referrer" />
 
-  <style>
-    #festivalGreeting {
-      font-size: 16px;
-      color: #ff9800;
-      font-weight: bold;
-      text-align: center;
-      margin: 20px;
-    }
-
-
-    .table-scroll {
-    max-height: 480px;  
-    overflow-y: auto;
-}
-
-.table-scroll thead th {
-    position: sticky;
-    top: 0;
-    background: #f8f9fa;
-    z-index: 1;
-}
-
-
-
-
-
-    .balloon {
-      width: 20px;
-      height: 30px;
-      background-color: red;
-      border-radius: 50% 50% 50% 50%;
-      position: absolute;
-      animation: float 5s infinite;
-    }
-
-    @keyframes float {
-      0% {
-        transform: translateY(0) rotate(0deg);
-      }
-
-      50% {
-        transform: translateY(-300px) rotate(180deg);
-      }
-
-      100% {
-        transform: translateY(0) rotate(360deg);
-      }
-    }
-  </style>
+    <link rel="stylesheet" href="{{ asset('css/shantachatterjee.css') }}">
 
 
 
